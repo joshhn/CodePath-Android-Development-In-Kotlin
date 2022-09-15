aeca3d9c5c73076fa13bdfa207ae0eb7e34d291d
joshhn <duy.huynhngoc.05@gmail.com>
2022-09-15 15:41:11 -0400

    Completed all required features Project 1

--- Unit 1 - Kotlin and Layouts/Wordle/app/src/main/java/com/joshhn/wordle/MainActivity.kt
+++ Unit 1 - Kotlin and Layouts/Wordle/app/src/main/java/com/joshhn/wordle/MainActivity.kt
@@ -1,33 +1,73 @@
 package com.joshhn.wordle
 
-import androidx.appcompat.app.AppCompatActivity
+
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.WindowInsetsControllerCompat
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.joshhn.wordle.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private var wordToGuess : String ?= null
+    private var binding: ActivityMainBinding? = null
     private val getWord = FourLetterWordList()
+    private var wordToGuess = ""
+    private var counter = 0
+    private var answer = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
 
-        hideSystemBars()
-
+        //Generate keyword
         wordToGuess = getWord.getRandomFourLetterWord()
-    }
+        binding?.tvKeyword?.text = wordToGuess
 
-    private fun hideSystemBars() {
-        val windowInsetsController =
-            ViewCompat.getWindowInsetsController(window.decorView) ?: return
-        // Configure the behavior of the hidden system bars
-        windowInsetsController.systemBarsBehavior =
-            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-        // Hide both the status bar and the navigation bar
-        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
+        binding?.btnSubmit?.setOnClickListener {
+            //Take input from user
+            val editText = binding?.etInput
+            val inputValue = editText?.text.toString().uppercase()
+
+            if(inputValue.length > 3){
+                //TODO: Implement checking function
+                counter++
+
+                hideKeyboard()
+                editText?.setText("")
+
+                answer = checkGuess(inputValue)
+                if(counter == 1){
+                    binding?.tvGuess1?.text = inputValue
+                    binding?.tvCheck1?.text = answer
+                    binding?.tvGuess1?.visibility = View.VISIBLE
+                    binding?.tvCheckLabel1?.visibility = View.VISIBLE
+                    binding?.tvCheck1?.visibility = View.VISIBLE
+                }else if(counter == 2){
+                    binding?.tvGuess2?.text = inputValue
+                    binding?.tvCheck2?.text = answer
+                    binding?.tvLabel2?.visibility = View.VISIBLE
+                    binding?.tvGuess2?.visibility = View.VISIBLE
+                    binding?.tvCheckLabel2?.visibility = View.VISIBLE
+                    binding?.tvCheck2?.visibility = View.VISIBLE
+                }else if(counter == 3) {
+                    binding?.tvGuess3?.text = inputValue
+                    binding?.tvCheck3?.text = answer
+                    binding?.tvLabel3?.visibility = View.VISIBLE
+                    binding?.tvGuess3?.visibility = View.VISIBLE
+                    binding?.tvCheckLabel3?.visibility = View.VISIBLE
+                    binding?.tvCheck3?.visibility = View.VISIBLE
+                    binding?.tvKeyword?.visibility = View.VISIBLE
+                    binding?.btnSubmit?.isClickable = false
+                }
+            }else {
+                Toast.makeText(this, "Keyword must contain 4 letters", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     /**
@@ -40,6 +80,7 @@ class MainActivity : AppCompatActivity() {
      *   '+' represents the right letter in the wrong place
      *   'X' represents a letter not in the target word
      */
+
     private fun checkGuess(guess: String) : String {
         var result = ""
         for (i in 0..3) {
@@ -54,5 +95,13 @@ class MainActivity : AppCompatActivity() {
             }
         }
         return result
+    }
+
+    private fun hideKeyboard() {
+        val view = this.currentFocus
+        if (view != null) {
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
     }
 }
