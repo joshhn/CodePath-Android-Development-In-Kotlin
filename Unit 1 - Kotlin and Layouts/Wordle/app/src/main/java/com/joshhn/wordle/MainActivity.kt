04df2d16fdcaca773b02f81948fb9ca61368dc47
joshhn <duy.huynhngoc.05@gmail.com>
2022-09-15 20:10:09 -0400

    Add stretch features

--- Unit 1 - Kotlin and Layouts/Wordle/app/src/main/java/com/joshhn/wordle/MainActivity.kt
+++ Unit 1 - Kotlin and Layouts/Wordle/app/src/main/java/com/joshhn/wordle/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.joshhn.wordle
 
 
-import android.app.Activity
 import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.github.jinatonic.confetti.CommonConfetti
 import com.joshhn.wordle.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,57 +22,114 @@ class MainActivity : AppCompatActivity() {
     private val getWord = FourLetterWordList()
     private var wordToGuess = ""
     private var counter = 0
-    private var answer = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
-        //Generate keyword
-        wordToGuess = getWord.getRandomFourLetterWord()
-        binding?.tvKeyword?.text = wordToGuess
+        binding?.btnStart?.setOnClickListener {
+            //Generate keyword
+            wordToGuess = getWord.getRandomFourLetterWord()
+            binding?.tvKeyword?.text = wordToGuess
+            counter = 0
+
+            binding?.btnStart?.visibility = View.INVISIBLE
+            binding?.etInput?.visibility = View.VISIBLE
+            binding?.btnSubmit?.visibility = View.VISIBLE
+            binding?.tvLabel1?.visibility = View.VISIBLE
+        }
 
         binding?.btnSubmit?.setOnClickListener {
             //Take input from user
             val editText = binding?.etInput
             val inputValue = editText?.text.toString().uppercase()
 
-            if(inputValue.length > 3){
-                //TODO: Implement checking function
+            if(inputValue.length > 3 && isValidInput(inputValue)){
                 counter++
 
                 hideKeyboard()
                 editText?.setText("")
 
-                answer = checkGuess(inputValue)
+                checkWin(inputValue)
+
+                val answer = checkGuess(inputValue)
+                val modifiedAnswer = customCheckedGuess(answer, inputValue)
+
                 if(counter == 1){
                     binding?.tvGuess1?.text = inputValue
-                    binding?.tvCheck1?.text = answer
+                    binding?.tvCheck1?.text = modifiedAnswer
                     binding?.tvGuess1?.visibility = View.VISIBLE
                     binding?.tvCheckLabel1?.visibility = View.VISIBLE
                     binding?.tvCheck1?.visibility = View.VISIBLE
                 }else if(counter == 2){
                     binding?.tvGuess2?.text = inputValue
-                    binding?.tvCheck2?.text = answer
+                    binding?.tvCheck2?.text = modifiedAnswer
                     binding?.tvLabel2?.visibility = View.VISIBLE
                     binding?.tvGuess2?.visibility = View.VISIBLE
                     binding?.tvCheckLabel2?.visibility = View.VISIBLE
                     binding?.tvCheck2?.visibility = View.VISIBLE
                 }else if(counter == 3) {
+                    checkWin(inputValue)
                     binding?.tvGuess3?.text = inputValue
-                    binding?.tvCheck3?.text = answer
+                    binding?.tvCheck3?.text = modifiedAnswer
                     binding?.tvLabel3?.visibility = View.VISIBLE
                     binding?.tvGuess3?.visibility = View.VISIBLE
                     binding?.tvCheckLabel3?.visibility = View.VISIBLE
                     binding?.tvCheck3?.visibility = View.VISIBLE
                     binding?.tvKeyword?.visibility = View.VISIBLE
                     binding?.btnSubmit?.isClickable = false
+                    binding?.btnRestart?.visibility = View.VISIBLE
                 }
             }else {
-                Toast.makeText(this, "Keyword must contain 4 letters", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Keyword must contain 4 letters and be from A to Z only", Toast.LENGTH_SHORT).show()
             }
         }
+
+        binding?.btnRestart?.setOnClickListener {
+            setupRestart()
+        }
+    }
+
+    private fun isValidInput(inputValue: String): Boolean {
+        for(i in 0..3){
+            if(inputValue[i] < 'A' || inputValue[i] > 'Z' ){
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun checkWin(inputValue: String) {
+        if(counter < 4 && inputValue == wordToGuess){
+            CommonConfetti.rainingConfetti(binding?.root, intArrayOf(Color.GREEN,Color.BLACK, Color.YELLOW, Color.RED, Color.GRAY, Color.WHITE, Color.BLUE, Color.CYAN, Color.DKGRAY, Color.LTGRAY, Color.MAGENTA))
+                .stream(5000)
+            binding?.btnSubmit?.isClickable = false
+            binding?.btnRestart?.visibility = View.VISIBLE
+            hideKeyboard()
+            Toast.makeText(this, "Congratulations! You win!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun setupRestart() {
+        binding?.btnStart?.visibility = View.VISIBLE
+        binding?.etInput?.visibility = View.INVISIBLE
+        binding?.btnSubmit?.visibility = View.INVISIBLE
+        binding?.tvLabel1?.visibility = View.INVISIBLE
+        binding?.tvLabel2?.visibility = View.INVISIBLE
+        binding?.tvLabel3?.visibility = View.INVISIBLE
+        binding?.tvCheckLabel1?.visibility = View.INVISIBLE
+        binding?.tvCheckLabel2?.visibility = View.INVISIBLE
+        binding?.tvCheckLabel3?.visibility = View.INVISIBLE
+        binding?.tvCheck1?.visibility = View.INVISIBLE
+        binding?.tvCheck2?.visibility = View.INVISIBLE
+        binding?.tvCheck3?.visibility = View.INVISIBLE
+        binding?.tvGuess1?.visibility = View.INVISIBLE
+        binding?.tvGuess2?.visibility = View.INVISIBLE
+        binding?.tvGuess3?.visibility = View.INVISIBLE
+        binding?.tvKeyword?.visibility = View.INVISIBLE
+        binding?.btnRestart?.visibility = View.INVISIBLE
+        binding?.btnSubmit?.isClickable = true
     }
 
     /**
@@ -83,6 +145,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun checkGuess(guess: String) : String {
         var result = ""
+
         for (i in 0..3) {
             if (guess[i] == wordToGuess[i]) {
                 result += "O"
@@ -95,6 +158,26 @@ class MainActivity : AppCompatActivity() {
             }
         }
         return result
+    }
+
+    private fun customCheckedGuess(checkedGuess: String, guess: String) : SpannableString {
+        val spannableString = SpannableString(guess)
+        val correctFColor = ForegroundColorSpan(ContextCompat.getColor(this@MainActivity, R.color.correctColor))
+        val wrongFColor = ForegroundColorSpan(ContextCompat.getColor(this@MainActivity, R.color.wrongColor))
+        val existFColor = ForegroundColorSpan(ContextCompat.getColor(this@MainActivity, R.color.existColor))
+
+        for (i in 0..3) {
+            if (checkedGuess[i] == 'O') {
+                spannableString.setSpan(correctFColor,i,i+1,Spannable.SPAN_INCLUSIVE_EXCLUSIVE)
+            }
+            else if (checkedGuess[i] == '+') {
+                spannableString.setSpan(existFColor,i,i+1,Spannable.SPAN_INCLUSIVE_EXCLUSIVE)
+            }
+            else{
+                spannableString.setSpan(wrongFColor,i,i+1,Spannable.SPAN_INCLUSIVE_EXCLUSIVE)
+            }
+        }
+        return spannableString
     }
 
     private fun hideKeyboard() {
