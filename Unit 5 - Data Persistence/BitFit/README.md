e15a9a4ed114a66d947f9db8d8b4daa10405dcd9
joshhn <duy.huynhngoc.05@gmail.com>
2022-10-15 15:39:13 -0400

    Complete Project 5

---
+++ Unit 5 - Data Persistence/BitFit/README.md
@@ -0,0 +1,62 @@
+# Android Project 5 - *BitFit*
+
+Submitted by: **Duy Ngoc Huynh**
+
+**BitFit** is a health metrics app that allows users to track ... [TODO] 
+
+Time spent: **8** hours spent in total
+
+## Required Features
+
+The following **required** functionality is completed:
+
+- [x] **At least one health metric is tracked (based on user input)**
+  - Chosen metric(s): `[TODO: FILL ME IN TO GET CREDIT]`
+- [x] **There is a "create entry" UI that prompts users to make their daily entry**
+- [x] **New entries are saved in a database and then updated in the RecyclerView**
+- [x] **On application restart, previously entered entries are preserved (i.e., are *persistent*)**
+ 
+The following **optional** features are implemented:
+
+- [x] **Create a UI for tracking averages and trends in metrics**
+- [x] **Improve and customize the user interface through styling and coloring**
+- [x] **Implement orientation responsivity**
+- [x] **Add a daily photo feature**
+
+The following **additional** features are implemented:
+
+- [ ] List anything else that you can get done to improve the app functionality!
+
+## Video Walkthrough
+
+Here's a walkthrough of implemented user stories:
+
+<img src='walkthrough.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+
+<!-- Replace this with whatever GIF tool you used! -->
+GIF created with ... 
+Google Pixel 3 
+<!-- Recommended tools:
+[Kap](https://getkap.co/) for macOS
+[ScreenToGif](https://www.screentogif.com/) for Windows
+[peek](https://github.com/phw/peek) for Linux. -->
+
+## Notes
+
+Describe any challenges encountered while building the app.
+
+## License
+
+    Copyright [2022] [Duy Ngoc Huynh]
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
--- Unit 5 - Data Persistence/BitFit/app/src/main/java/com/joshhn/bitfit/MainActivity.kt
+++ Unit 5 - Data Persistence/BitFit/app/src/main/java/com/joshhn/bitfit/MainActivity.kt
@@ -1,11 +1,139 @@
 package com.joshhn.bitfit
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.RequestParams
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.joshhn.bitfit.databinding.ActivityMainBinding
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
+import okhttp3.Headers
+import org.json.JSONException
+import org.json.JSONObject
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+    private lateinit var imageUrl: String
+    private var foodList: MutableList<Food> = mutableListOf()
+    private var totalCaloriesAte: Double = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+//        val client = AsyncHttpClient()
+//        val params = RequestParams()
+//
+//        client.get("https://api.thecatapi.com/v1/images/search",params, object : JsonHttpResponseHandler() {
+//            override fun onFailure(
+//                statusCode: Int,
+//                headers: Headers?,
+//                response: String?,
+//                throwable: Throwable?
+//            ) {
+//                Log.e("Cat Image Json", "Failed to fetch cat image: $statusCode")
+//            }
+//
+//            override fun onSuccess(statusCode: Int, headers: Headers, json: JSON) {
+//                Log.i("Cat Image Json", "Successfully fetched cat image: $json")
+//                try {
+//                    val resultsJSON : JSONObject = json.jsonObject as JSONObject
+//                    imageUrl = resultsJSON.get("url").toString()
+//
+//                    Log.i("json", resultsJSON.toString())
+//
+//                } catch (e: JSONException) {
+//                    Log.e("Cat Image Json", "Exception: $e")
+//                }
+//            }
+//        })
+
+        val radius = 40; // corner radius, higher value = more rounded
+        val margin = 10; // crop margin, set to 0 for corners with no crop
+        Glide.with(this@MainActivity)
+            .load(R.drawable.sample)
+            .apply(RequestOptions.bitmapTransform(RoundedCornersTransformation(radius,margin)))
+            .into(binding?.ivDailyPhoto!!)
+
+
+        var inputActivityResultLauncher: ActivityResultLauncher<Intent> = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                    val newFood: Food = result.data?.getSerializableExtra("new food") as Food
+                    foodList.add(newFood)
+                    totalCaloriesAte += newFood.totalCalories?.toDouble()!!
+
+                    binding?.tvSummary?.text = "Total calories needed per day: 2000 kcal\\nTotal calories you ate today: $totalCaloriesAte kcal"
+                    lifecycleScope.launch(IO) {
+                        (application as FoodApplication).db.foodDao().deleteAll()
+                        (application as FoodApplication).db.foodDao().insertAll(foodList.map {
+                            FoodEntity(
+                                date = it.date,
+                                foodName = it.foodName,
+                                amount = it.amount,
+                                caloriesUnit = it.caloriesUnit,
+                                calories = it.totalCalories
+                            )
+                        })
+                    }
+                }
+            }
+
+        fun startInputActivity() {
+            val intent = Intent(this@MainActivity, InputActivity::class.java)
+            inputActivityResultLauncher.launch(intent)
+        }
+
+        binding?.btnInsert?.setOnClickListener {
+            startInputActivity()
+        }
+
+        val foodAdapter = FoodAdapter(this@MainActivity, foodList)
+        binding?.rvNutrition?.adapter = foodAdapter
+        binding?.rvNutrition?.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+
+        lifecycleScope.launch {
+            (application as FoodApplication).db.foodDao().getAll().collect { databaseList ->
+                databaseList.map { entity ->
+                    Food(
+                        entity.date,
+                        entity.foodName,
+                        entity.caloriesUnit,
+                        entity.amount,
+                        entity.calories
+                    )
+                }.also { mappedList ->
+                    foodList.clear()
+                    foodList.addAll(mappedList)
+                    foodAdapter.notifyDataSetChanged()
+                }
+            }
+        }
+
+        binding?.btnDelete?.setOnClickListener {
+            totalCaloriesAte = 0.0
+            lifecycleScope.launch(IO) {
+                (application as FoodApplication).db.foodDao().deleteAll()
+            }
+            binding?.tvSummary?.text = "Total calories needed per day: 2000 kcal\\nTotal calories you ate today: $totalCaloriesAte kcal"
+        }
     }
 }
