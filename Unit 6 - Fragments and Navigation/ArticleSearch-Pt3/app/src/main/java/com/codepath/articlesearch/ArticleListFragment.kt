e7ae57928f89738527e3f5dc71ed56677c470237
joshhn <duy.huynhngoc.05@gmail.com>
2022-10-21 15:19:04 -0400

    Add lab 6

---
+++ Unit 6 - Fragments and Navigation/ArticleSearch-Pt3/app/src/main/java/com/codepath/articlesearch/ArticleListFragment.kt
@@ -0,0 +1,86 @@
+package com.codepath.articlesearch
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import org.json.JSONException
+import okhttp3.Headers
+
+private const val TAG = "ArticleListFragment"
+private const val SEARCH_API_KEY = BuildConfig.API_KEY
+private const val ARTICLE_SEARCH_URL =
+    "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=${SEARCH_API_KEY}"
+
+class ArticleListFragment : Fragment() {
+
+    private val articles = mutableListOf<Article>()
+    private lateinit var articlesRecyclerView: RecyclerView
+    private lateinit var articleAdapter: ArticleAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Change this statement to store the view in a variable instead of a return statement
+        val view = inflater.inflate(R.layout.fragment_article_list, container, false)
+
+        // Add these configurations for the recyclerView and to configure the adapter
+        val layoutManager = LinearLayoutManager(context)
+        articlesRecyclerView = view.findViewById(R.id.articles)
+        articlesRecyclerView.layoutManager = layoutManager
+        articlesRecyclerView.setHasFixedSize(true)
+        articleAdapter = ArticleAdapter(view.context, articles)
+        articlesRecyclerView.adapter = articleAdapter
+
+        // Update the return statement to return the inflated view from above
+        return view
+    }
+
+    companion object {
+        fun newInstance(): ArticleListFragment {
+            return ArticleListFragment()
+        }
+    }
+
+    private fun fetchArticles() {
+        val client = AsyncHttpClient()
+        client.get(ARTICLE_SEARCH_URL, object : JsonHttpResponseHandler() {
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                response: String?,
+                throwable: Throwable?
+            ) {
+                Log.e(TAG, "Failed to fetch articles: $statusCode")
+            }
+
+            override fun onSuccess(statusCode: Int, headers: Headers, json: JSON) {
+                Log.i(TAG, "Successfully fetched articles: $json")
+                try {
+                    val parsedJson = createJson().decodeFromString(
+                        SearchNewsResponse.serializer(),
+                        json.jsonObject.toString()
+                    )
+                    parsedJson.response?.docs?.let { list ->
+                        articles.addAll(list)
+                        articleAdapter.notifyDataSetChanged()
+                    }
+                } catch (e: JSONException) {
+                    Log.e(TAG, "Exception: $e")
+                }
+            }
+
+        })
+    }
+}
